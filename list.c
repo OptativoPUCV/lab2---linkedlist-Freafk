4f7fb42895733b0800da69f6446e005481e81679
Replit user <>
2023-03-22 01:16:55 +0000

    new try

--- list.c
+++ list.c
@@ -41,28 +41,17 @@ List * createList() {
 
 void * firstList(List * list) {
 
-  if(list!=NULL)
-  {
-  list->current=list->head;
-  return list->current->data;
-  }
   return NULL;
 }
 
 void * nextList(List * list) {
 
-  list->current=list->current->next;
-
-  return list->current->data;
+  return NULL;
 }
 
 void * lastList(List * list) {
 
-  while(list->current->next!=NULL)
-    {
-      list->current=list->current->next;
-    }
-  return list->current->data;
+  return NULL;
 }
 
 void * prevList(List * list) {
